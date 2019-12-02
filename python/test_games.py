b3ff3ebb6ea57f0e07022a7a872ed2047e0cd1bc
Ben Little <ben.little6@gmail.com>
2019-12-02 16:32:48 -0700

    Add tests for TTTState fidelity.

--- python/test_games.py
+++ python/test_games.py
@@ -3,6 +3,7 @@ import numpy as np
 from games import NimState, TTTState
 from ttt import BEGIN
 
+
 def test_NimState():
     assert NimState(25, num_takeaway=5) == NimState(25, num_takeaway=5) 
     assert NimState(25, num_takeaway=3) == NimState(25) 
@@ -25,10 +26,37 @@ def test_TTTState():
          [0,0,1],
          [1,0,2]]
     ))
+    assert s == TTTState(
+        np.array(
+            [[1,0,0],
+             [0,0,1],
+             [1,0,0]]
+        ),
+        np.array(
+            [[0,0,2],
+             [0,0,0],
+             [0,0,2]]
+        ),
+    )
     assert not s.is_terminal
-    return
+    assert TTTState.from_array(np.array(
+        [[1,0,2],
+         [0,1,2],
+         [1,0,2]]
+    )).is_terminal
+    assert TTTState.from_array(np.array(
+        [[1,0,2],
+         [0,1,2],
+         [0,0,1]]
+    )).is_terminal
+    s = TTTState.from_array(BEGIN)
     for _ in range(100):
-        s = TTTState(BEGIN)
+        s = TTTState.from_array(BEGIN)
         while not s.is_terminal:
-            s_ = s.get_random_next_state()
+            try:
+                s_ = s.get_random_next_state()
+            except IndexError:
+                print(s)
+                print(s_)
             assert s_ in list(s.next_states)
+            s = s_
